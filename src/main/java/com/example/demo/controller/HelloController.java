a00e3649dee491054cb2e7bcdc0b8f71796d4f74
icrms-tstack <jeemy@7span.com>
2025-07-19 14:46:23 +0530

    Initial commit from downloaded project

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/user")
+    public String hello() {
+        return "Hello Java Users welcome to the workshopss122333390000";
+    }
+}
