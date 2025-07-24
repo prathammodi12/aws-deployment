6b130a55674066b21a1420b1f1012d366b9db4f6
jeemy-7span <jeemy@7span.com>
2025-07-24 22:25:17 +0530

    Update BuildSpec

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.DeleteMapping;
+//import com.example.demo.model.User;
+//import com.example.demo.service.UserService;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +18,11 @@ import java.util.Map;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private UserService userService;
-
-    UserController(UserService userService){
-        this.userService = userService;
-    }
+//    private UserService userService;
+//
+//    UserController(UserService userService){
+//        this.userService = userService;
+//    }
 
     @GetMapping
     public Map<String,String> hello() {
@@ -31,18 +31,18 @@ public class UserController {
         return map;
     }
 
-    @PostMapping("/save")
-    public User saveUser(@RequestBody User user) {
-        return userService.saveUser(user);
-    }
-
-    @GetMapping("/fetch")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-    }
+//    @PostMapping("/save")
+//    public User saveUser(@RequestBody User user) {
+//        return userService.saveUser(user);
+//    }
+//
+//    @GetMapping("/fetch")
+//    public List<User> getAllUsers() {
+//        return userService.getAllUsers();
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteUser(@PathVariable Long id) {
+//        userService.deleteUser(id);
+//    }
 }
