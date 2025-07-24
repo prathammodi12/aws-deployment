1dd46a91286fa53ee8c1c5606725434b10badccf
jeemy-7span <jeemy@7span.com>
2025-07-24 14:47:28 +0530

    Update BuildSpec

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 public class HelloController {
-    @GetMapping("/user")
+    @GetMapping
     public String hello() {
-        return "Hello Java Users welcome to the workshop330";
+        return "Hello Java Users welcome to the board";
+    }
+
+    @GetMapping("/role")
+    public String getUserRole(){
+        return "You are Admin!";
     }
 }
